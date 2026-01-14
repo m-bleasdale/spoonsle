0b0edbed7a01476c56ec94f604de2b1cb644feab
Morgan Bleasdale <morganbleasdale234@gmail.com>
2026-01-14 10:56:22 +0000

    general UI Improvements

--- components/game.tsx
+++ components/game.tsx
@@ -15,8 +15,9 @@ export default function Game ({data, selectedIndex, imageOrder} : {data : Pub[],
     const images = data[selectedIndex].images;
 
     const [currentImage, setCurrentImage] = useState(0);
+    const [numberOfImages, setNumberOfImages] = useState(1);
     const next = () =>
-        setCurrentImage((prev) => (prev < mistakes.length ? prev + 1 : prev));
+        setCurrentImage((prev) => (prev < numberOfImages - 1 ? prev + 1 : prev));
     const prev = () =>
         setCurrentImage((prev) => (prev > 0 ? prev - 1 : prev));
 
@@ -25,13 +26,15 @@ export default function Game ({data, selectedIndex, imageOrder} : {data : Pub[],
             setMistakes(prev => [...prev, guess]);
             const items = guess.split(",").map(s => s.trim());
 
-            setCurrentImage(mistakes.length);
-
             setPubs(pubs.filter(pub =>
                 !(pub.name === items[0] && pub.area === items[1] && pub.city === items[2])
             ));
 
             if(mistakes.length + 1 === 5) setGameOver(true);
+            else {
+                setCurrentImage(currentImage + 1);
+                setNumberOfImages(numberOfImages + 1);
+            }
 
         }
         else {
@@ -42,29 +45,29 @@ export default function Game ({data, selectedIndex, imageOrder} : {data : Pub[],
 
     return (
         <div className="w-80 md:w-150 flex flex-col items-center gap-5">
-            <div className="flex gap-2 flex-col w-80 md:w-150 h-120 rounded-md">
+            <div className="flex gap-2 flex-col h-100 w-80 md:w-110 rounded-md">
                 <img
                     src={images[imageOrder[currentImage]]}
                     alt="Image to guess"
-                    className="object-cover h-110 w-full border-2 rounded-md"
+                    className="object-cover h-80 w-80 md:w-110 border-2 rounded-md"
                 />
 
-                {mistakes.length > 0 && (
-                    <div className="flex gap-2 justify-center items-center">
-                        {currentImage + 1}
-                        /
-                        {mistakes.length + 1}
-                        <Button
-                            variant="outline"
-                            onClick={prev}
-                        >
-                            Previous
-                        </Button>
-                        <Button
-                            onClick={next}
-                        >
-                            Next
-                        </Button>
+                {numberOfImages > 1 && (
+                    <div className="flex flex-col items-center text-sm gap-2">
+                        Image {currentImage + 1} of {numberOfImages}
+                        <div className="flex gap-2 justify-center items-center">
+                            <Button
+                                variant="outline"
+                                onClick={prev}
+                            >
+                                Previous
+                            </Button>
+                            <Button
+                                onClick={next}
+                            >
+                                Next
+                            </Button>
+                        </div>
                     </div>
                 )}
 
@@ -73,16 +76,16 @@ export default function Game ({data, selectedIndex, imageOrder} : {data : Pub[],
             {mistakes.map((mistake, i) => (
                 <div 
                     key={i}
-                    className="rounded-md flex flex-row w-80 md:w-110 h-15 px-5 bg-red-700 text-white font-medium items-center justify-between text-lg"
+                    className="rounded-md flex flex-row w-80 md:w-110 h-15 px-5 bg-red-700 text-white  items-center justify-between text-md"
                 >
                     {mistake}
-                    <X size={32} strokeWidth={4} color="#ffffff"/>
+                    <X size={28} strokeWidth={3} color="#ffffff"/>
                 </div>
             ))}
 
             {(gameOver && win) &&
                 <div 
-                    className="rounded-md flex flex-row w-80 md:w-110 h-15 px-5 bg-green-700 text-white font-medium items-center justify-between text-lg"
+                    className="rounded-md flex flex-row w-80 md:w-110 h-15 px-5 bg-green-700 text-white items-center justify-between text-md"
                 >
                     {`${data[selectedIndex].name}, ${data[selectedIndex].area}, ${data[selectedIndex].city}`}
                     <Check size={32} strokeWidth={4} color="#ffffff"/>
@@ -91,7 +94,7 @@ export default function Game ({data, selectedIndex, imageOrder} : {data : Pub[],
 
             {(gameOver && !win) &&
                 <div 
-                    className="border-2 rounded-md flex bg-white flex-row w-80 md:w-110 h-15 px-5 items-center justify-between text-lg"
+                    className="border-2 rounded-md flex bg-white flex-row w-80 md:w-110 h-15 px-5 items-center justify-between text-md"
                 >
                     <p>It was <span className="font-bold">{`${data[selectedIndex].name}, ${data[selectedIndex].area}, ${data[selectedIndex].city}`}</span></p>
                 </div>
@@ -114,7 +117,7 @@ export default function Game ({data, selectedIndex, imageOrder} : {data : Pub[],
             [...Array(5 - 1 - mistakes.length)].map((_, i) => (
                 <div
                     key={i}
-                    className="border-2 rounded-md flex flex-row w-80 md:w-110 h-15 bg-white text-white font-medium items-center justify-between text-lg"
+                    className="border-2 rounded-md flex flex-row w-80 md:w-110 h-15 bg-white text-white items-center justify-between text-md"
                 />
             ))}
 
